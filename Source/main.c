cd916469676a307ae0d082f2efd416b137ce176c
yuru108 <Changapple0736@gmail.com>
2023-03-02 11:29:13 +0800

    1

--- Source/main.c
+++ Source/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-//#include "search.h"
+#include "search.h"
 
 int main()
 {
@@ -8,14 +8,14 @@ int main()
 
     // Static library
 
-    // int arr[] = { 2, 3, 4, 10, 40 };
+    int arr[] = { 2, 3, 4, 10, 40 };
 
-    // int n = sizeof(arr) / sizeof(arr[0]);
-    // int x = 10;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 10;
 
-    // int result = binary_search(arr, 0, n - 1, x);
-    // (result == -1) ? printf("Element is not present in array\n")
-    //     : printf("Element is present at index %d\n", result);
+    int result = binary_search(arr, 0, n - 1, x);
+    (result == -1) ? printf("Element is not present in array\n")
+        : printf("Element is present at index %d\n", result);
 
     // Debug in Visual Studio
 
